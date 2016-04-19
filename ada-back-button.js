97afdc451668a446e7cff36ab63ead91c9ac88df
william <william@adamantio.com.br>
2016-04-18 21:05:20 -0300

    criacao do projeto

---
+++ ada-back-button.js
@@ -0,0 +1,61 @@
+backButton = angular.module('ada-back-button',[])
+	.value('callbacks',[])
+	.value('defaultCallback',[])
+	.value('globalCallbacks',[])
+	.factory('BackButton',function(callbacks,defaultCallback,globalCallbacks,$location){
+
+			return{
+
+				/**
+				 * Add a default backbutton handler, which is called when 
+				 * a matching callback is not found in the callbacks array.
+				 */
+				setDefaultCallback : function(callback){
+					defaultCallback[0] = callback;
+				},
+
+				/**
+				 * Register a global callback that is invocked every time a back button is pressed,
+				 * even if a matching callback is found. The global callbacks are called first and,
+				 * if one of then returns true, the callback chain stops and none of the other callbacks
+				 * is called anymore.
+				 */
+				addGlobalCallback : function(callback){
+					globalCallbacks.push(callback);
+				},
+
+				/**
+				 * Add a callback for the location
+				 */ 
+				addEvent : function(location, callback){
+					callbacks[location] = callback;
+				},
+
+				/**
+				 * Add a callback for the current location
+				 */ 
+				addEvent : function(callback){
+					callbacks[$location.path()] = callback;
+				}
+			};
+
+		})
+	.run(function(callbacks,defaultCallback,globalCallbacks,$location){
+		var onBackButton = function(){
+
+			for (var i in globalCallbacks){
+				if (globalCallbacks[i]()) return;
+			}
+
+			if (callbacks[$location.path()]){
+				callbacks[$location.path()]();
+			}else{
+				if (defaultCallback && defaultCallback[0])
+					defaultCallback[0]();
+			}			
+		};
+
+		$(document).on("backbutton", onBackButton);
+	});
+
+	
